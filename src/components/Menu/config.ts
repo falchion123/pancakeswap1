7d65a59b508f1849fd50e99a3d934ff75f152373
rick <506322829@qq.com>
2022-02-11 15:19:50 +0800

    pancake-swwap-interface-v1 testnet

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,34 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    status: {
+      text: 'MIGRATE',
+      color: 'warning',
+    },
+    items: [
+      {
+        label: 'Exchange',
+        href: '/swap',
+      },
+      {
+        label: 'Liquidity',
+        href: '/pool',
+      }
+    ],
+  }
+
+
+
+
+]
+
+export default config
